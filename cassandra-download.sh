79f056adf33acdb4fd3b1056b565f4b4a450de99
Subham Rakshit <developersubham@gmail.com>
2017-05-17 22:24:20 +0100

    Remvoed hardcoded cassandra version from download script

--- cassandra-download.sh
+++ cassandra-download.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-CASSANDRA_VERSION=2.1.17
-
 echo "Downloading and unzipping apache cassandra-$CASSANDRA_VERSION"
 mirror=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred')
 url="$mirror/cassandra/$CASSANDRA_VERSION/apache-cassandra-${CASSANDRA_VERSION}-bin.tar.gz"
