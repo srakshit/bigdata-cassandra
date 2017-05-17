2045594d4b6a2d17ef1e72941d9efeb10344ab05
Subham Rakshit <developersubham@gmail.com>
2017-05-17 14:53:54 +0100

    Added an extr space before property values

--- cassandra-properties.sh
+++ cassandra-properties.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #set cassandra cassandra.yaml
-sed -i -e "s|^cluster_name:.*$|cluster_name:'DEV Cassandra Cluster'|" $CASSANDRA_HOME/conf/cassandra.yaml
-sed -i -e "s|^listen_address:.*$|listen_address:`hostname -i`|" $CASSANDRA_HOME/conf/cassandra.yaml
+sed -i -e "s|^cluster_name:.*$|cluster_name: 'DEV Cassandra Cluster'|" $CASSANDRA_HOME/conf/cassandra.yaml
+sed -i -e "s|^listen_address:.*$|listen_address: `hostname -i`|" $CASSANDRA_HOME/conf/cassandra.yaml
