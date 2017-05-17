37c2be3559627180ff65fa15d5cf009b2bd075dd
Subham Rakshit <developersubham@gmail.com>
2017-05-17 21:30:12 +0100

    Added rpc_address and seeds

--- cassandra-properties.sh
+++ cassandra-properties.sh
@@ -3,3 +3,5 @@
 #set cassandra cassandra.yaml
 sed -i -e "s|^cluster_name:.*$|cluster_name: 'DEV Cassandra Cluster'|" $CASSANDRA_HOME/conf/cassandra.yaml
 sed -i -e "s|^listen_address:.*$|listen_address: `hostname -i`|" $CASSANDRA_HOME/conf/cassandra.yaml
+sed -i -e "s|^rpc_address:.*$|rpc_address: `hostname -i`|" $CASSANDRA_HOME/conf/cassandra.yaml
+sed -ri "s/(- seeds:).*/\1 \"`hostname -i`\"/" $CASSANDRA_HOME/conf/cassandra.yaml
