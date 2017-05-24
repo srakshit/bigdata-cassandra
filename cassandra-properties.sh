5549384ee988a94b4fa4e7400d4d5b797ab9c256
Subham Rakshit <developersubham@gmail.com>
2017-05-24 16:56:45 +0100

    rpc_Address is changed to 0.0.0.0 and broadcast_rpc_address is added

--- cassandra-properties.sh
+++ cassandra-properties.sh
@@ -3,5 +3,7 @@
 #set cassandra cassandra.yaml
 sed -i -e "s|^cluster_name:.*$|cluster_name: 'DEV Cassandra Cluster'|" $CASSANDRA_HOME/conf/cassandra.yaml
 sed -i -e "s|^listen_address:.*$|listen_address: `hostname -i`|" $CASSANDRA_HOME/conf/cassandra.yaml
-sed -i -e "s|^rpc_address:.*$|rpc_address: `hostname -i`|" $CASSANDRA_HOME/conf/cassandra.yaml
+sed -i -e "s|^rpc_address:.*$|rpc_address: 0.0.0.0|" $CASSANDRA_HOME/conf/cassandra.yaml
+sed -i -e "s|^# broadcast_rpc_address:.*$|broadcast_rpc_address: `hostname -i`|" $CASSANDRA_HOME/conf/cassandra.yaml
+
 sed -ri "s/(- seeds:).*/\1 \"`hostname -i`\"/" $CASSANDRA_HOME/conf/cassandra.yaml
